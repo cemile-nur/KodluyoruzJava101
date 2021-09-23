96ac142ea826cb72a75de989581db57e3c4fe6cb
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-09-23 15:52:56 +0300

    Taksimetre Hesaplama

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int a,b;
-        double c;
+        int km;
+        double perKm= 2.20, total,startPrice=10 ;
         Scanner input= new Scanner(System.in);
-        System.out.print("1. sayıyı Giriniz : ");
-        a= input.nextInt();
-        System.out.print("2. sayıyı Giriniz : ");
-        b= input.nextInt();
+        System.out.print("mesafeyi km cinsinden giriniz : ");
+        km= input.nextInt();
 
-        c= Math.sqrt((a*a)+(b*b));
-        System.out.print("Hipotenüs :  "+ c);
+        total= (km*perKm);
+        total+= startPrice;
+
+        total= (total<20) ? 20: total;
+        System.out.print("Toplam Tutar : "+total);
+
 
     }
 }
