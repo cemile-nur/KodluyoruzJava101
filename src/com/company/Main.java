d88064e66aff5bb9883cbe8943f7dc1ece991bd4
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-09-23 23:21:03 +0300

    Ödev - Manav Kasa Programı

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int kilo;
-        double boy,indeks=0 ;
+        int armutKilo, elmaKilo, domatesKilo, muzKilo, patlicanKilo;
+        double armut=2.14, elma=3.67, domates=1.11, muz=0.95, patlican=5, toplam ;
 
         Scanner input= new Scanner(System.in);
 
-        System.out.print("Lütfen Boyunuzu Metre Cinsinden Giriniz : ");
-        boy= input.nextDouble();
+        System.out.print("Armut Kaç Kilo : ");
+        armutKilo= input.nextInt();
+        System.out.print("Elma Kaç Kilo : ");
+        elmaKilo= input.nextInt();
+        System.out.print("Domates Kaç Kilo : ");
+        domatesKilo= input.nextInt();
+        System.out.print("Muz Kaç Kilo : ");
+        muzKilo= input.nextInt();
+        System.out.print("Patlican Kaç Kilo : ");
+        patlicanKilo= input.nextInt();
 
-        System.out.print("Lütfen Kilonuzu Giriniz : ");
-        kilo= input.nextInt();
-
-        indeks= kilo/(boy*boy);
-        System.out.print("Vücut Kitle İndeksiniz : " + indeks);
-
+        toplam= (armutKilo*armut)+(elmaKilo*elma)+(domatesKilo*domates)+(muzKilo*muz)+(patlicanKilo*patlican);
+        System.out.print("Toplam Tutar: " + toplam);
 
     }
 }
