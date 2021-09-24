856dcf1995ef6ac6377ae39e7efc01f0f571373f
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-09-24 15:56:31 +0300

    Sınıfı Geçme Durumu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,27 +5,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String kullaniciAdi="patika", kullaniciSifre="java159", kullaniciSifreYeni, cevap, ad, sifre;
-        Scanner kullanici = new Scanner(System.in);
+        int mat,fizik,kimya,turkce,muzik;
 
-        System.out.print("Kullanıcı Adınız: ");
-        ad = kullanici.nextLine();
-        System.out.print("Şifreniz: ");
-        sifre = kullanici.nextLine();
-        if(sifre.equals(kullaniciSifre) && ad.equals(kullaniciAdi)){
-            System.out.print("Giriş Yapıldı !");
-        }else
-            System.out.print("Yanlış Bilgi Girdiniz! Şifrenizi Yenilemek İster misiniz? ");
+        Scanner input=new Scanner(System.in);
 
-        cevap=kullanici.nextLine();
-        if (cevap.equals("evet")){
-            System.out.print("Yeni Şifre Giriniz: ");
-            kullaniciSifreYeni=kullanici.nextLine();
-            if (kullaniciSifreYeni.equals(kullaniciSifre) || kullaniciSifreYeni.equals(sifre)){
-                System.out.print("Girdiğiniz Şifre Uygun Değil !!");
-            }else
-                System.out.print("Şifre Kaydedildi !");
-        }else
-            System.out.print("Yeniden Giriş Yapın.");
+        System.out.print("Matematik Notunuz: ");
+        mat = input.nextInt();
+
+        System.out.print("Türkçe Notunuz: ");
+        turkce = input.nextInt();
+
+        System.out.print("Fizik Notunuz: ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya Notunuz: ");
+        kimya = input.nextInt();
+
+        System.out.print("Müzik Notunuz: ");
+        muzik = input.nextInt();
+
+        double avarage = (mat+fizik+kimya+turkce+muzik)/5;
+
+        if(avarage>0 && avarage<55){
+            System.out.print("Not Ortalamanız: "+avarage+" Kaldınız !");
+        }else if (avarage>=55 && avarage<=100){
+            System.out.print("Not Ortalamanız: "+avarage+" Tebrikler Geçtiniz !");
+        }else {
+            System.out.print("Girdiğiniz Notları Kontrol ediniz!" );
+        }
+
     }
 }
