47e5cab40adb697ab6122ee14ad2e8eaa230f904
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-09-25 14:54:40 +0300

    Girilen Sayıya Kadar Olan Çift Sayıları Bulan Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,35 +4,34 @@ import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int sayi1;
+        Scanner hesapla = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        sayi1 = hesapla.nextInt();
 
-        int yıl;
-        Scanner input =new Scanner(System.in);
-
-        System.out.print("Yıl Giriniz: ");
-        yıl= input.nextInt();
-
-        if(yıl % 4==0){
-            System.out.print( yıl +" bir artık yıldır ! ");
-
-        }else if((yıl %100==0) && (yıl %400==0)){
-
-            System.out.print( yıl +" bir artık yıldır ! ");
-
-        }else if (yıl == 1700 && yıl==1800 && yıl ==1900){
-
-            System.out.print( yıl +" bir artık yıl değildir ! ");
-
-        }else {
-            System.out.print( yıl +" bir artık yıl değildir ! ");
+        for(int i=0; i<= sayi1; i+=2){
+            System.out.print(i + ",");
         }
 
+        System.out.print(" ******************");
 
+        int sayi,i,sayac=0,toplami=0,ortalama=0;
+        Scanner inp= new Scanner(System.in);
 
+        System.out.print(" Sayı girin");
+        sayi= inp.nextInt();
 
+        for ( i=0; i<=sayi; i++){
 
+            if((i % 3==0) || (i % 4==0)){
 
+                toplami+=i;
+                sayac++;
+            }
+        }
 
-
+        ortalama=toplami/sayac;
+        System.out.print("ortalama= "+ortalama);
 
     }
 }
