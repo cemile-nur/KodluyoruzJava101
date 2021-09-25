18e479d6c9c83f0a3e731580f9e8d913844b759a
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-09-25 12:30:26 +0300

    Ödev - Çin Zodyağı Hesaplama

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,72 +5,59 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Tanımlama
-        int mesafe, yas, tip;
-        double mesafeUcreti=0.10,normalTutar,indirimliTutar,yasIndirim,tipTutar=0.20, tipIndirim;
+        int dogumTarihi;
+        Scanner input =new Scanner(System.in);
 
-        //Kullanıcıdan veri alma ve değişkenlere atama.
-        Scanner bilet = new Scanner(System.in);
-        System.out.print("Gideceğiniz Mesafe (KM): ");
-        mesafe = bilet.nextInt();
-        System.out.print("Yaşınız: ");
-        yas = bilet.nextInt();
-        System.out.print("Yolculuk Tipi (1:Tek Yön, 2:Gidiş-Dönüş): ");
-        tip = bilet.nextInt();
+        System.out.print("Doğum Tarihinizi Giriniz: ");
+        dogumTarihi= input.nextInt();
 
-        //Bilet ücreti hesaplama
-        normalTutar=mesafe*mesafeUcreti;
-        if(mesafe>0 && yas>0){
-            if (yas<=12){
-                switch (tip){
-                    case 1:
-                        yasIndirim=(normalTutar*50)/100;
-                        indirimliTutar=normalTutar-yasIndirim;
-                        System.out.print("%50 indirim. Fiyat: "+indirimliTutar+" ₺");
-                        break;
-                    case 2:
-                        yasIndirim=(normalTutar*50)/100;
-                        indirimliTutar=normalTutar-yasIndirim;
-                        tipIndirim=indirimliTutar*tipTutar;
-                        System.out.print("%50+%20 indirim. Fiyat: "+tipIndirim+" ₺");
-                        break;
-                }
-            }else if (yas>12 && yas<=24){
-                switch (tip){
-                    case 1:
-                        yasIndirim=(normalTutar*10)/100;
-                        indirimliTutar=normalTutar-yasIndirim;
-                        System.out.print("%10 indirim. Fiyat: "+indirimliTutar+" ₺");
-                        break;
-                    case 2:
-                        yasIndirim=(normalTutar*10)/100;
-                        indirimliTutar=normalTutar-yasIndirim;
-                        tipIndirim=indirimliTutar*tipTutar;
-                        System.out.print("%10+%20 indirim. Fiyat: "+tipIndirim+" ₺");
-                        break;
-                }
-            }else if (yas>=65){
-                switch (tip){
-                    case 1:
-                        yasIndirim=(normalTutar*30)/100;
-                        indirimliTutar=normalTutar-yasIndirim;
-                        System.out.print("%30 indirim. Fiyat: "+indirimliTutar+" ₺");
-                        break;
-                    case 2:
-                        yasIndirim=(normalTutar*30)/100;
-                        indirimliTutar=normalTutar-yasIndirim;
-                        tipIndirim=indirimliTutar*tipTutar;
-                        System.out.print("%30+%20 indirim. Fiyat: "+tipIndirim+" ₺");
-                        break;
-                }
-            }else if (tip==2){
-                indirimliTutar=normalTutar*tipTutar;
-                System.out.print("%20 indirim. Fiyat: "+indirimliTutar+" ₺");
-            }else {
-                System.out.print("Fiyat: "+normalTutar+" ₺");
-            }
-        }else {
-            System.out.print("Hatalı Değer Girdiniz !");
+        if(dogumTarihi % 12==0){
+
+            System.out.print("Çin Zodyağı Burcunuz :Maymun");
+
+        }else if (dogumTarihi % 12==1){
+            System.out.print("Çin Zodyağı Burcunuz :Horoz");
+
+        } else if (dogumTarihi % 12==2){
+            System.out.print("Çin Zodyağı Burcunuz :Köpek");
+
+        }else if (dogumTarihi % 12==3){
+            System.out.print("Çin Zodyağı Burcunuz :Domuz");
+
+        }else if (dogumTarihi % 12==4){
+            System.out.print("Çin Zodyağı Burcunuz :Fare");
+
+        }else if (dogumTarihi % 12==5){
+            System.out.print("Çin Zodyağı Burcunuz :Öküz");
+
+        }else if (dogumTarihi % 12==6){
+            System.out.print("Çin Zodyağı Burcunuz :Kaplan");
+
+        }else if (dogumTarihi % 12==7){
+            System.out.print("Çin Zodyağı Burcunuz :Tavşan");
+
+        }else if (dogumTarihi % 12==8){
+            System.out.print("Çin Zodyağı Burcunuz :Ejderha");
+
+        }else if (dogumTarihi % 12==9){
+            System.out.print("Çin Zodyağı Burcunuz :Yılan");
+
+        }else if (dogumTarihi % 12==10){
+            System.out.print("Çin Zodyağı Burcunuz :At");
+
+        }else if (dogumTarihi % 12==11) {
+            System.out.print("Çin Zodyağı Burcunuz :Koyun");
         }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
