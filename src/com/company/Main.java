73c6f92ca177dca35d05b8010c53db89ca12e182
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-10-02 02:50:15 +0300

    Ödev - Fibonacci Serisi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,20 @@ import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
-        for(int sayi=2;sayi<=100;sayi++)
+        Scanner reader = new Scanner(System.in);
+        int n1=0,n2=1,n3,i,n;
+        System.out.print("N Sayısını Girin:");
+        n = reader.nextInt();
+        System.out.print(n1+" "+n2);//0 ve 1 yazdır
+
+        for(i=2;i<n;++i)//Döngü 2 den başlıyor çünkü ilk 2 terim 0 ve 1 her zaman yazılacak
         {
-            int kontrol = 0;
-            for (int i = 2; i < sayi; i++)
-            {
-                if (sayi % i == 0)
-                {
-                    kontrol = 1;
-                    break;
-                }
-            }
-
-            if(kontrol==0)
-            {
-                System.out.print(sayi + " ");
-
-            }
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
         }
-
+        System.out.println();
     }
 
 }
