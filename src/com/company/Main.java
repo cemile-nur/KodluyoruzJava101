1ce2ebb83753c4abcf4df36fbe12a9f46ec6de9c
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-10-11 02:27:16 +0300

    Dizideki Elemanların Ortalamasını Bulan Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        Employe personel = new Employe("Cemile Nur Keklik ",5000 ,45 ,2000);
-        personel.printPerson();
+        double[] list = {1, 2, 3, 4, 5,6,7};
+        double harmonikSeriToplami = 0.0;
+        double harmonikSeriOrtalamasi;
+
+        for (double j : list) {
+            harmonikSeriToplami += (1 / j);
+        }
+
+        harmonikSeriOrtalamasi = list.length / harmonikSeriToplami;
+
+        System.out.println("HARMONİK SERİ TOPLAMI \t\t: " + harmonikSeriToplami);
+        System.out.println("HARMONİK SERİ ORTALAMASI \t: " + harmonikSeriOrtalamasi);
+
     }
 }
 
