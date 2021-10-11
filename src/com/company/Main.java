d0d7e3d538e4e121360fd1a45df7e157b932c5de
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-10-12 01:12:53 +0300

    Dizideki Elemanların Max ve Min Değerlerini Bulan Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,40 @@
+import java.util.Scanner;
 
+import  java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double[] list = {1, 2, 3, 4, 5,6,7};
-        double harmonikSeriToplami = 0.0;
-        double harmonikSeriOrtalamasi;
+        int n;
 
-        for (double j : list) {
-            harmonikSeriToplami += (1 / j);
+        Scanner input = new Scanner(System.in);
+        System.out.print("sayı girin: ");
+        n=input.nextInt();
+
+        int[] list = {15,12,788,1,-1,-778,2,0};
+
+        int min = n;
+        int max = n;
+
+        for (int i : list) {
+
+            if(i>n){
+                if(n==max || i<max){
+                    max=i;
+                }
+            }
+            if(i<n){
+                if(n==min || i>min){
+                    min=i;
+                }
+            }
         }
 
-        harmonikSeriOrtalamasi = list.length / harmonikSeriToplami;
-
-        System.out.println("HARMONİK SERİ TOPLAMI \t\t: " + harmonikSeriToplami);
-        System.out.println("HARMONİK SERİ ORTALAMASI \t: " + harmonikSeriOrtalamasi);
+        String list1 = Arrays.toString(list);
+        System.out.println("Dizi : " + list1);
+        System.out.println("Girilen sayı: "+ n);
+        System.out.println("Girilen sayıdan küçük en yakın sayı : " + min);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : " + max);
 
     }
-}
+    }
 
