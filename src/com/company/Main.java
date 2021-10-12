2b080f024d10a1b6f6918eff14bb95d1aa87aee7
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-10-12 22:49:13 +0300

    Dizideki Tekrar Eden Sayıları Bulan Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,28 +3,42 @@ import java.util.Scanner;
 import  java.util.Arrays;
 
 public class Main {
+
+    static boolean isFind( int[] arr, int value){
+
+        for (int i : arr){
+            if(i==value){
+                return  true;
+            }
+        }
+        return  false;
+    }
     public static void main(String[] args) {
 
-        String[][] letter = new String[7][4];
+        int [] list = { 7,8,9,58,69,33,13,58,88,9,104,88,13};
+        int[] even = new int[list.length];
+        int startIndex=0;
 
-        for (int i = 0; i < letter.length; i++) {
-            for (int j = 0; j < letter[i].length; j++) {
-                if (i == 0 || i == 3 || i==6) {
-                    letter[i][j] = " * ";
-                } else if (j == 0 || j == 3) {
-                    letter[i][j] = " * ";
-                } else {
-                    letter[i][j] = "   ";
+        for(int i=0; i< list.length; i++){
+            for(int j=0; j< list.length; j++){
+
+                if((i != j) && (list[i] == list[j])){
+                    if(!isFind(even, list[i])){
+                        if( list[i] % 2==0){
+                        even[startIndex++]= list[i];
+                        }
+                    }
+                    break;
                 }
+
+            }
+        }
+        for (int value: even){
+            if(value != 0){
+                System.out.println(value);
             }
         }
 
-        for (String[] row : letter){
-            for (String col : row){
-                System.out.print(col);
-            }
-            System.out.println();
-        }
     }
  }
 
